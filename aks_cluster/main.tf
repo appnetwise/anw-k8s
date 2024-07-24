e3684f3d8b3e96bfd36215e4078713374e302130
skmdmsk <shaik.quimuddin@appnetwise.com>
2024-07-24 18:46:53 +0530

    added lb osdisktype zones in aks module and also some variables

--- aks_cluster/main.tf
+++ aks_cluster/main.tf
@@ -25,16 +25,25 @@ module "aks" {
   resource_group_name           = var.rg_name
   location                      = var.rg_location
   kubernetes_version            = var.kubernetes_version # Specify the desired Kubernetes version here
+
+  #node_pool_profile
   node_pool_name                = var.node_pool_name
   node_count                    = var.node_count
   vm_size                       = var.vm_size
   subnet_id                     = module.vnet.subnet_id
   os_sku                        = var.os_sku
   os_disk_size_gb               = var.os_disk_size_gb
+
+  os_disk_type                  = var.os_disk_type
+  availability_zones            = var.availability_zones
+
+  #enable_auto_scaling_for_node_pool
   enable_auto_scaling           = var.enable_auto_scaling
   min_count                     = var.min_count
   max_count                     = var.max_count
   max_pods                      = var.max_pods
+
+  #network_profile
   network_plugin                = var.network_plugin 
   network_policy                = var.network_policy
   service_cidr                  = var.service_cidr
@@ -42,6 +51,7 @@ module "aks" {
   private_cluster_enabled       = var.private_cluster_enabled
   identity_type                 = var.identity_type
   tags_environment              = var.tags_environment
+  load_balancer_sku             = var.load_balancer_sku
 
   depends_on = [
     module.rg,
